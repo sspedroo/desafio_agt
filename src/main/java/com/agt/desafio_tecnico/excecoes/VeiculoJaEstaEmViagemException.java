506d9eefcacf03c65e0327c4e383bbcbd0b705f8
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-17 22:46:41 -0300

    feat: adiciona exceção para conflito de dados quando veículo já está em viagem

---
+++ src/main/java/com/agt/desafio_tecnico/excecoes/VeiculoJaEstaEmViagemException.java
@@ -0,0 +1,7 @@
+package com.agt.desafio_tecnico.excecoes;
+
+public class VeiculoJaEstaEmViagemException extends ConflitoDeDadosException {
+    public VeiculoJaEstaEmViagemException(String message) {
+        super(message);
+    }
+}
