b05aa2a32742f6b9e5612f787e0c9ad547173614
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-20 19:23:10 -0300

    feat: adiciona suporte a cache para listagem de veículos e invalida cache em operações de registro

---
+++ src/main/java/com/agt/desafio_tecnico/infra/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package com.agt.desafio_tecnico.infra;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Desafio Técnico - API",
+                version = "1.0.0",
+                description = "API para gerenciamento de viagens, funcionários e veículos.",
+                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                        name = "Pedro Oliveira",
+                        email = "012pedropaulo@gmail.com")
+        ))
+public class OpenApiConfig {
+}
