b45659055f1aac821c562b51b77500ada87d6d7d
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-19 21:25:29 -0300

    feat: adiciona exceção FuncionarioJaEmViagemException para tratamento de conflitos

---
+++ src/main/java/com/agt/desafio_tecnico/excecoes/FuncionarioJaEmViagemException.java
@@ -0,0 +1,7 @@
+package com.agt.desafio_tecnico.excecoes;
+
+public class FuncionarioJaEmViagemException extends ConflitoDeDadosException {
+    public FuncionarioJaEmViagemException(String message) {
+        super(message);
+    }
+}
