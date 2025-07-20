35db94e3535271c42105b0bde362b84abb830a80
Pedro Oliveira <012pedropaulo@gmail.com>
2025-07-19 21:34:38 -0300

    feat: adiciona suporte a cache com Spring, incluindo dependências necessárias

--- src/main/java/com/agt/desafio_tecnico/DesafioTecnicoApplication.java
+++ src/main/java/com/agt/desafio_tecnico/DesafioTecnicoApplication.java
@@ -2,12 +2,14 @@ package com.agt.desafio_tecnico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableCaching
 public class DesafioTecnicoApplication {
 
 	public static void main(String[] args) {
